fa55b4f60e593adddc2faabdfdc523befa62b7b2
thatchlobe <thatchlobe@outlook.com>
2023-04-23 18:18:53 +0800

    利用匿名管道实现父子进程通信，实现类似ps aux | grep 的功能

---
+++ train/parent-child-ipd.c
@@ -0,0 +1,50 @@
+/*
+实现类似 ps aux | grep 的效果
+子进程执行 ps aux ，通过管道把数据发送到父进程进行提取
+
+*/
+
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <string.h>
+
+int main(){
+    int ret;
+    
+    //创建管道
+    int pipefd[2];
+    ret = pipe(pipefd);
+    if (ret < 0){
+        perror("pipe");
+        return -1;
+    }
+
+    //创建子进程
+    pid_t pid = fork();
+    if(pid > 0){
+        //父进程
+        close(pipefd[1]);//关闭写端fd
+        char buf[1024] = {};
+        while(1){
+            bzero(buf, 1024);
+            int len = read(pipefd[0], buf, sizeof(buf)-1);
+            printf("%s", buf);
+        }
+
+    }else if(pid == 0){
+        //子进程
+        close(pipefd[0]);//关闭读端fd
+        dup2(pipefd[1], STDOUT_FILENO);//重定向标准输出
+        execlp("ps", "ps", "aux", NULL);//调用ps aux
+        //这两句在exec调用成功后不会执行
+        perror("exec");
+        return 0;
+    }else{
+        perror("fork");
+        return -1;
+    }
+
+    return 0;
+
+}
