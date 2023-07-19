db296e37b6d25eb44288b8848f70bd3fb3d8dfa9
vm <thatchlobe@outlook.com>
2023-07-19 09:13:50 +0800

    eventloop

--- src/EventLoop.h
+++ src/EventLoop.h
@@ -1,1 +1,25 @@
+#ifndef EVENTLOOPHEAD
+#define EVENTLOOPHEAD
+
+
+#include <vector>
 #include "Channel.h"
+#include "Poller.h"
+
+class EventLoop{
+public:
+    EventLoop();
+    ~EventLoop();
+
+private:
+    Poller poller_;
+    Channel listenChannel_;
+    std::vector<Channel> eventList_;
+
+
+};
+
+
+
+
+#endif
