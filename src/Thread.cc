07c2ad6c106bcfa0bd655db9c27f4398dbede9da
alu <thatchlobe@outlook.com>
2023-09-07 15:25:30 +0800

    Thread简单类

---
+++ src/Thread.cc
@@ -0,0 +1,44 @@
+#include "Thread.h"
+
+#include <semaphore.h>
+
+#include "CurrentThread.h"
+std::atomic_int32_t numCreated_ = 0;
+
+Thread::Thread(ThreadFunc func, const std::string& name)
+    : started_(false), joined_(false), tid_(0), func_(std::move(func)), name_(name) {
+    setDefaultName;
+}
+Thread::~Thread() {
+    if (started_ && !joined_) {
+        thread_->detach();  // thread类提供的线程分离
+    }
+}
+
+void Thread::start() {
+    started_ = true;
+    sem_t sem;
+    sem_init(&sem, false, 0);
+
+    thread_ = std::shared_ptr<std::thread>(new std::thread([&]() {
+        tid_ = CurrentThread::tid();  // 获取线程的tid
+        sem_post(&sem);               // 获取到tid了，线程完成初始化，tid可以访问，信号量+1
+        func_();                      // 开启新线程，执行线程函数
+    }));
+
+    sem_wait(&sem);  // 如果信号量为0，则没完成初始化，start()不能返回
+}
+
+void Thread::join() {
+    joined_ = true;
+    thread_->join();
+}
+
+void Thread::setDefaultName() {
+    int num = ++numCreated_;
+    if (name_.empty()) {
+        char buf[32] = {};
+        snprintf(buf, sizeof(buf), "Thread%d", num);
+        name_ = buf;
+    }
+}
---
+++ src/Thread.h
@@ -0,0 +1,40 @@
+#pragma once
+#pragma once
+#include <atomic>
+#include <functional>
+#include <memory>
+#include <string>
+#include <thread>
+
+#include "noncopyable.h"
+
+class Thread : noncopyable {
+public:
+    using ThreadFunc = std::function<void()>;
+
+    explicit Thread(ThreadFunc, const std::string& name = std::string());
+    ~Thread();
+
+    void start();
+    void join();
+
+    bool started() const { return started_; }
+    pid_t tid() const { return tid_; }
+    const std::string& name() const { return name_; }
+
+    static int numCreated() { return numCreated_; }
+
+private:
+    void setDefaultName();
+
+    bool started_;
+    bool joined_;
+
+    std::shared_ptr<std::thread> thread_;  // 用共享指针封装的线程
+    pid_t tid_;                            // tid
+    ThreadFunc func_;                      // 线程执行的函数
+
+    std::string name_;  // 线程名称
+
+    static std::atomic_int32_t numCreated_;  // 启动的线程数
+};
